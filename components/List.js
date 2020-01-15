d939af57d3d99b0ec15bb204357c8f254a158a3e
Rasmus Karling <rasmusjk@metropolia.fi>
2020-01-15 12:14:10 +0200

    task a complete

--- components/List.js
+++ components/List.js
@@ -1,20 +1,26 @@
 import {FlatList} from "react-native";
-import React from "react";
-import PropTypes from "prop-types"
+import React, {useContext} from "react";
+import PropTypes from "prop-types";
 import ListItem from "./ListItem";
+import {MediaContext} from '../contexts/MediaContext';
+import {useFetch} from '../Hooks/APIHooks';
 
-const List = (props) => {
-  console.log(props);
+const List = () => {
+  const [media, setMedia] = useContext(MediaContext);
+  const [data, loading] =useFetch('https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json');
+  console.log('List',data,loading);
+  setMedia(data);
   return (
     <FlatList
-      data={props.mediaArray}
+      data={media}
+      keyExtractor={(item, index) => index.toString()}
       renderItem={({item}) => <ListItem singleMedia={item} />}
     />
   );
 };
 
-List.propTypes = {
+/*List.propTypes = {
   mediaArray: PropTypes.array,
-};
+};*/
 
 export default List;
